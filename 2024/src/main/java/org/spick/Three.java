bc591987dd441b4fd350fd8775a949cdea5dcf97
Florian Widmer <florian.widmer@gmail.com>
2024-12-04 08:27:29 +0100

    three refactored

--- 2024/src/main/java/org/spick/Three.java
+++ 2024/src/main/java/org/spick/Three.java
@@ -3,14 +3,13 @@ package org.spick;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Three extends AbstractPuzzle<Integer> {
-
-    private boolean enabled = true;
-    private Pattern REGEX = Pattern.compile("(do\\(\\))|(don't\\(\\))|(mul\\((\\d{1,3}),(\\d{1,3})\\))");
 
     public Three(String input) {
         super(input);
@@ -24,45 +23,53 @@ public class Three extends AbstractPuzzle<Integer> {
 
     @Override
     public Integer first() throws IOException, URISyntaxException {
-        var matcher = getMatcher();
-        var multiplications = new ArrayList<Integer>();
-        while (matcher.find()) {
-            multiplications.add(nextInt(matcher, false));
-        }
-        return multiplications.stream()
+        return new Parser(false, streamLines()).parse().stream()
                 .mapToInt(a -> a)
                 .sum();
-    }
-
-    private Matcher getMatcher() {
-        var input = streamLines().collect(Collectors.joining());
-        
-        var matcher = REGEX.matcher(input);
-        return matcher;
     }
 
     @Override
     public Integer second() throws IOException, URISyntaxException {
-        var matcher = getMatcher();
-        var multiplications = new ArrayList<Integer>();
-        enabled = true;
-        
-        while (matcher.find()) {
-            multiplications.add(nextInt(matcher, true));
-        }
-        return multiplications.stream()
+        return new Parser(true, streamLines()).parse().stream()
                 .mapToInt(a -> a)
                 .sum();
     }
 
-    private Integer nextInt(Matcher matcher, boolean conditional){
-        if (conditional && matcher.group(1) != null) {
-            enabled = true;
-        } else if (conditional && matcher.group(2) != null) {
-            enabled = false;
-        } else if (enabled && matcher.group(3) != null) {
-            return Integer.parseInt(matcher.group(4)) * Integer.parseInt(matcher.group(5));
+    private class Parser {
+        private boolean conditional;
+        private boolean enabled = true;
+        private Pattern REGEX = Pattern.compile("(do\\(\\))|(don't\\(\\))|(mul\\((\\d{1,3}),(\\d{1,3})\\))");
+        private CharSequence input;
+
+        public Parser(boolean conditional, Stream<String> input) {
+            this.conditional = conditional;
+            this.input = input.collect(Collectors.joining());
         }
-        return 0;
+
+        private Matcher getMatcher() {
+            var matcher = REGEX.matcher(input);
+            return matcher;
+        }
+
+        public List<Integer> parse() {
+            var matcher = getMatcher();
+            enabled = true;
+            var multiplications = new ArrayList<Integer>();
+            while (matcher.find()) {
+                multiplications.add(nextInt(matcher));
+            }
+            return multiplications;
+        }
+
+        private Integer nextInt(Matcher matcher) {
+            if (conditional && matcher.group(1) != null) {
+                enabled = true;
+            } else if (conditional && matcher.group(2) != null) {
+                enabled = false;
+            } else if (enabled && matcher.group(3) != null) {
+                return Integer.parseInt(matcher.group(4)) * Integer.parseInt(matcher.group(5));
+            }
+            return 0;
+        }
     }
 }
