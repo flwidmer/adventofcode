3106ea13952c1d1a6a726ddd64e24ddbafb7b146
Florian Widmer <florian.widmer@gmail.com>
2024-12-04 08:14:34 +0100

    three refactor

--- 2024/src/main/java/org/spick/Three.java
+++ 2024/src/main/java/org/spick/Three.java
@@ -3,11 +3,14 @@ package org.spick;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.function.Function;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Three extends AbstractPuzzle<Integer> {
+
+    private boolean enabled = true;
+    private Pattern REGEX = Pattern.compile("(do\\(\\))|(don't\\(\\))|(mul\\((\\d{1,3}),(\\d{1,3})\\))");
 
     public Three(String input) {
         super(input);
@@ -21,37 +24,45 @@ public class Three extends AbstractPuzzle<Integer> {
 
     @Override
     public Integer first() throws IOException, URISyntaxException {
-        var input = streamLines().collect(Collectors.joining());
-        var regex = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
-        var matcher = regex.matcher(input);
+        var matcher = getMatcher();
         var multiplications = new ArrayList<Integer>();
         while (matcher.find()) {
-            multiplications
-                    .add(Integer.parseInt(matcher.group(1)) * Integer.parseInt(matcher.group(2)));
+            multiplications.add(nextInt(matcher, false));
         }
         return multiplications.stream()
                 .mapToInt(a -> a)
                 .sum();
     }
 
+    private Matcher getMatcher() {
+        var input = streamLines().collect(Collectors.joining());
+        
+        var matcher = REGEX.matcher(input);
+        return matcher;
+    }
+
     @Override
     public Integer second() throws IOException, URISyntaxException {
-        var input = streamLines().collect(Collectors.joining());
-        var regex = Pattern.compile("(do\\(\\))|(don't\\(\\))|(mul\\((\\d{1,3}),(\\d{1,3})\\))");
-        var matcher = regex.matcher(input);
+        var matcher = getMatcher();
         var multiplications = new ArrayList<Integer>();
-        var enabled = true;
+        enabled = true;
+        
         while (matcher.find()) {
-            if (matcher.group(1) != null) {
-                enabled = true;
-            } else if (matcher.group(2) != null) {
-                enabled = false;
-            } else if (enabled && matcher.group(3) != null) {
-                multiplications.add(Integer.parseInt(matcher.group(4)) * Integer.parseInt(matcher.group(5)));
-            }
+            multiplications.add(nextInt(matcher, true));
         }
         return multiplications.stream()
                 .mapToInt(a -> a)
                 .sum();
+    }
+
+    private Integer nextInt(Matcher matcher, boolean conditional){
+        if (conditional && matcher.group(1) != null) {
+            enabled = true;
+        } else if (conditional && matcher.group(2) != null) {
+            enabled = false;
+        } else if (enabled && matcher.group(3) != null) {
+            return Integer.parseInt(matcher.group(4)) * Integer.parseInt(matcher.group(5));
+        }
+        return 0;
     }
 }
